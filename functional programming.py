92888fb8e922258b0b00e3403b4eaecbc4cd53e1
YebiDepriansyah <131685598+YebiDepriansyah@users.noreply.github.com>
2023-04-24 21:38:35 +0700

    Update functional programming.py

--- functional programming.py
+++ functional programming.py
@@ -5,9 +5,8 @@ def multiply_by_2(n):
     return n * 2
 
 def map_and_filter(numbers):
-    # Menggunakan fungsi is_even dan multiply_by_2 untuk melakukan filter dan mapping pada list numbers
     return list(map(multiply_by_2, filter(is_even, numbers)))
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = map_and_filter(numbers)
-print(result)  # Output: [4, 8, 12, 16, 20]
+print(result) 
